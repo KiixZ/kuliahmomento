56875c8f50f2cc3911fde6f0c9f466d8e4e96dc9
Kii <56679633+KiixZ@users.noreply.github.com>
2024-09-30 11:25:46 +0700

    Add files via upload
    kuliah sbd praktikum pertama

---
+++ prak1_sbd.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE sbd_a091;
+
+USE sbd_a091;
+
+CREATE TABLE satu(
+nim VARCHAR(10),
+nama VARCHAR (50),
+nilai INT)
+
+INSERT INTO satu VALUES ('24sa11a091','rifki saputra',99)
+SELECT * FROM satu
+DROP TABLE satu
